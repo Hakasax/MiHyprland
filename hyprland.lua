e74421056778bb105fb34b43edd3d038f55c1c19
Hakasax <jcabrini.sant@gmail.com>
2026-05-24 21:16:35 -0400

    Create hyprland.lua

---
+++ hyprland.lua
@@ -0,0 +1,132 @@
+----MONITORES
+hl.monitor({
+    output = "VGA-1",
+    mode = "1366x768@60",
+    scale = 1
+})
+hl.monitor({
+    output = "LVDS-1",
+    disabled = true
+})
+----APPS
+local archivos = "nautilus"
+local terminal = "kitty"
+local menu = "fuzzel"
+local web = "firefox"
+----BINDS
+local mod = "SUPER"
+hl.bind(mod .. " + Q", hl.dsp.exec_cmd(terminal))
+hl.bind(mod .. " + E", hl.dsp.exec_cmd(archivos))
+hl.bind(mod .. " + F", hl.dsp.exec_cmd(web))
+hl.bind(mod .. " + R", hl.dsp.exec_cmd(menu))
+hl.bind(mod .. " + C", hl.dsp.window.close())
+hl.bind(mod .. " + M", hl.dsp.exec_cmd("loginctl terminate-user \"$USER\""))
+hl.bind(mod .. " + left",  hl.dsp.focus({ direction = "left" }))
+hl.bind(mod .. " + right", hl.dsp.focus({ direction = "right" }))
+hl.bind(mod .. " + up",    hl.dsp.focus({ direction = "up" }))
+hl.bind(mod .. " + down",  hl.dsp.focus({ direction = "down" }))
+----SONIDO
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
+hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
+hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
+hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
+hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
+----AUTOINICIO
+hl.on("hyprland.start", function()
+    hl.exec_cmd("waybar &")
+    hl.exec_cmd("dunst")
+    hl.exec_cmd(terminal)
+    hl.exec_cmd("wal -R &")
+    hl.exec_cmd("awww-daemon")
+end)
+----VISUALES
+hl.config({
+    general = {
+	gaps_in = 5,
+	gaps_out = 15,
+	border_size = 4,
+	col = {
+	   active_border = "rgba(e86a5Fee)",
+	},
+	layout = "dwindle"	
+    },
+    decoration = {
+	rounding = 10,
+	rounding_power = 2,
+	active_opacity = 1.0,
+	inactive_opacity = 0.5,
+  	blur = {
+	   enabled = true,
+	   size = 6,
+	   passes = 2,
+	   vibrancy = 0.1696,
+	},
+    },
+    animations = {
+	enabled = true,
+    },
+})
+----TECLADO
+hl.config({
+    input = {
+	kb_layout = "latam",
+	follow_mouse = 1
+    }
+})
+----COSAS Q NOSE Q MRD SON PERO ESTAN EN EL EXAMPLE
+-- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
+hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
+hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
+hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
+hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
+hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
+
+-- Default springs
+hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+
+hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
+hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
+hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, spring = "easy" })
+hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 87%" })
+hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" })
+hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
+hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
+hl.animation({ leaf = "layers",        enabled = true,  speed = 3.81, bezier = "easeOutQuint" })
+hl.animation({ leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "easeOutQuint", style = "fade" })
+hl.animation({ leaf = "layersOut",     enabled = true,  speed = 1.5,  bezier = "linear",       style = "fade" })
+hl.animation({ leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" })
+hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 1.21, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "quick" })
+-- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
+hl.config({
+    dwindle = {
+        preserve_split = true, -- You probably want this
+    },
+})
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
+hl.config({
+    master = {
+        new_status = "master",
+    },
+})
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/ for more
+hl.config({
+    scrolling = {
+        fullscreen_on_one_column = true,
+    },
+})
+----WORKSPACES
+for i = 1, 10 do
+    local key = i % 10 -- 10 maps to key 0
+    hl.bind(mod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
+    hl.bind(mod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+end
