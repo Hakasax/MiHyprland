cbd1619a2919ffca00cd601d5983cc4fc03111dd
Hakasax <hakasax@proton.me>
2026-06-09 15:37:42 -0400

    modified:   ~/.config/hypr/hyprland.lua

--- ~/.config/hypr/hyprland.lua
+++ ~/.config/hypr/hyprland.lua
@@ -29,7 +29,8 @@ hl.bind(mod .. " + left",  hl.dsp.focus({ direction = "left" }))
 hl.bind(mod .. " + right", hl.dsp.focus({ direction = "right" }))
 hl.bind(mod .. " + up",    hl.dsp.focus({ direction = "up" }))
 hl.bind(mod .. " + down",  hl.dsp.focus({ direction = "down" }))
-----SONIDO
+hl.bind(mod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
+hl.bind(mod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
 hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
 hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
@@ -153,3 +154,9 @@ for i = 1, 10 do
     hl.bind(mod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
     hl.bind(mod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
 end
+
+local suppressMaximizeRule = hl.window_rule({
+    name  = "suppress-maximize-events",
+    match = { class = ".*" },
+    suppress_event = "maximize",
+})
